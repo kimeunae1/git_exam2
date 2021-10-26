55be8bb16c1bddfaea8612469e71ec7b0756d185
은애 <ksk78020@naver.com>
2021-10-26 09:41:48 +0900

    201914013-3

--- src/git_exam2/Hello.java
+++ src/git_exam2/Hello.java
@@ -2,6 +2,7 @@ package git_exam2;
 
 public class Hello {
 	public static void main(String[] args) {
+		System.out.println("IT융합자율학부");
 		System.out.println("201914013");
 		System.out.println("김은애");
 	}
